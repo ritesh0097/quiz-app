710c71f98b01c2057c60d93d8ace11fbc488804c
Ritesh Kumar <ritdev0097@gmail.com>
2024-10-13 20:23:39 +0530

    file added

--- src/questions/questions.js
+++ src/questions/questions.js
@@ -11,11 +11,11 @@ const Ques = [
     },
     {
         qn: 2,
-        ques: "Which planet is known as the Red Planet?",
-        opt1: "Venus",
-        opt2: "Mars",
-        opt3: "Jupiter",
-        opt4: "Saturn",
+        ques: "What is the national flower of India?",
+        opt1: "Rose",
+        opt2: "Lotus",
+        opt3: "Jasmine",
+        opt4: "Marigold",
 
         ans: 2,
     },
@@ -31,23 +31,23 @@ const Ques = [
     },
     {
         qn: 4,
-        ques: "What is the largest continent by area?",
-        opt1: "Africa",
-        opt2: "Europe",
-        opt3: "Asia",
-        opt4: "North America",
+        ques: 'Which city is known as the "Silicon Valley of India"?',
+        opt1: "Hyderabad",
+        opt2: "Chennai",
+        opt3: "Bangalore",
+        opt4: "Pune",
 
         ans: 3,
     },
     {
         qn: 5,
-        ques: "In which year did India gain independence?",
-        opt1: "1945",
-        opt2: "1947",
-        opt3: "1950",
-        opt4: "1952",
+        ques: 'Which river is known as the Ganga" of the South?',
+        opt1: "Godavari",
+        opt2: "Krishna",
+        opt3: "Kaveri",
+        opt4: "Tungabhadra",
 
-        ans: 2,
+        ans: 1,
     },
     {
         qn: 6,
@@ -59,6 +59,66 @@ const Ques = [
 
         ans: 2,
     },
+    {
+        qn: 7,
+        ques: "What is the largest state in India by area?",
+        opt1: "Maharashtra",
+        opt2: "Uttar Pradesh",
+        opt3: "Rajasthan",
+        opt4: "Madhya Pradesh",
+
+        ans: 3,
+    },
+    {
+        qn: 8,
+        ques: "Which Indian state is known for its tea plantations?",
+        opt1: "Assam",
+        opt2: "Kerala",
+        opt3: "West Bengal",
+        opt4: "Tamil Nadu",
+
+        ans: 1,
+    },
+    {
+        qn: 9,
+        ques: "Which Indian state is famous for its classical dance form Bharatanatyam?",
+        opt1: "Kerala",
+        opt2: "Karnataka",
+        opt3: "Tamil Nadu",
+        opt4: "Andhra Pradesh",
+
+        ans: 3,
+    },
+    {
+        qn: 10,
+        ques: "What is the main agricultural product of India?",
+        opt1: "Rice",
+        opt2: "Wheat",
+        opt3: "Sugarcane",
+        opt4: "Tea",
+
+        ans: 1,
+    },
+    {
+        qn: 11,
+        ques: 'Which Indian state is known as the "Land of Five Rivers"?',
+        opt1: "Rajasthan",
+        opt2: "Gujarat",
+        opt3: "Punjab",
+        opt4: "Haryana",
+
+        ans: 3,
+    },
+    {
+        qn: 12,
+        ques: 'Which city is known as the "Pink City" of India?',
+        opt1: "Jaipur",
+        opt2: "Agra",
+        opt3: "Delhi",
+        opt4: "Udaipur",
+
+        ans: 1,
+    }
 ]
 
 export default Ques;
